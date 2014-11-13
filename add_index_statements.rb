47d132439b7a301e1bc7d6c7a0da05c3eea6adf5
Favio Manriquez Leon <favio@favrik.com>
2014-11-12 21:23:26 -0800

    Add handling of duplicate index names

--- add_index_statements.rb
+++ add_index_statements.rb
@@ -1,11 +1,41 @@
+def index_name(statement)
+  statement.split(':name => ')[1].strip.gsub(/"/, '')
+end
+
+def index_name_increment(indexes, index_name)
+  original_index_name = index_name.dup
+  number = 1
+  while true
+    index_name = "#{original_index_name}_#{number.to_s}"
+    if indexes.include?(index_name)
+      number += 1
+    else
+      break
+    end
+  end
+
+  index_name
+end
+
+indexes = []
+
 ARGF.each_line.grep(/add_index/) do |index_statement|
   # Skip unique indexes because the lanyrd converter preserves them
-  next if index_statement.include?('unique: true')
+  next if index_statement.include?(':unique => true')
 
+  current_index = index_name(index_statement)
+  if indexes.include?(current_index)
+    old_index = current_index.dup
+    current_index = index_name_increment(indexes, current_index)
+
+    index_statement.sub!(":name => \"#{old_index}\"", ":name => \"#{current_index}\"")
+  end
+
+  indexes.push(current_index)
   puts <<EOS
 begin
   ActiveRecord::Base.connection.#{index_statement.strip}
-  puts 'Created index ' + %q{#{index_statement.split[4]}}
+  puts 'Created index ' + %q{#{current_index}}
 rescue ArgumentError => e
   puts e
 end
